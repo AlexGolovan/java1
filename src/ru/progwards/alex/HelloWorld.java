2dba6f61fe541f2c7e72394890e7da68e82af466
avgol <a.v.golovan@mail.ru>
2019-11-11 10:46:10 +0500

    HelloWorld start

--- src/ru/progwards/alex/HelloWorld.java
+++ src/ru/progwards/alex/HelloWorld.java
@@ -1,7 +1,13 @@
 package ru.progwards.alex;
 
 public class HelloWorld {
+    static void helloWorld(){
+        String str;
+        str = "Привет, мир!";
+        System.out.println(str);
+    }
     public static void main(String[] args){
-        System.out.println("Привет, Мир!");
+        helloWorld();
+
     }
 }
