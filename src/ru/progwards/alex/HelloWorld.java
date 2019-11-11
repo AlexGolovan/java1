a606ea295a6ae8f2bb462df28229142522768c63
avgol <a.v.golovan@mail.ru>
2019-11-11 10:55:50 +0500

    HelloWorld start

--- src/ru/progwards/alex/HelloWorld.java
+++ src/ru/progwards/alex/HelloWorld.java
@@ -4,9 +4,16 @@ public class HelloWorld {
     static void println(String str){
         System.out.println(str);
     }
+    static int addition(int x, int y){
+        int z = x + y;
+        return z;
+    }
     public static void main(String[] args){
         println("Привет, мир!");
         println("Мир, ты слышишь меня?!");
+        System.out.print("Я знаю, что 5 +7 =");
+        int z = addition(5,7);
+        System.out.println(z);
 
     }
 }
